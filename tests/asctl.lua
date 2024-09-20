7d84f40d1220c5ebb7a32f44e2491eb99088084f
ciripel <nedelcu.alexandru@yahoo.com>
2024-09-20 11:09:41 +0300

    test: asctl restart

--- tests/asctl.lua
+++ tests/asctl.lua
@@ -145,10 +145,6 @@ test["asctl - start"] = function()
     local result, err = new_test_env(options):run(function(env, ascendOutput)
         local startTime = os.time()
 
-        -- while true do
-        --     print(ascendOutput:read("l"))
-        -- end
-
         while true do
             local line = ascendOutput:read("l", 1, "s")
             if line and line:match("date started") then
@@ -178,6 +174,59 @@ test["asctl - start"] = function()
     end):result()
     test.assert(result, err)
 end
+
+test["asctl - restart"] = function()
+    ---@type AscendTestEnvOptions
+    local options = {
+        services = {
+            ["date"] = {
+                sourcePath = "assets/services/simple-date.hjson",
+            }
+        },
+        assets = {
+            ["scripts/date.lua"] = "assets/scripts/date.lua"
+        }
+    }
+
+    local result, err = new_test_env(options):run(function(env, ascendOutput)
+        local startTime = os.time()
+
+        while true do -- wait for service started
+            local line = ascendOutput:read("l")
+            if line and line:match("date started") then
+                break
+            end
+            if os.time() > startTime + 10 then
+                return false, "Service did not start in time"
+            end
+        end
+
+        -- stop the service
+        local ok, outputOrError = env:asctl({ "restart", "date" })
+        if not ok then
+            return false, outputOrError
+        end
+
+        local steps = 0 -- at 2 steps we consider test completed (stop + start)
+        while true do
+            local line = ascendOutput:read("l")
+            if line and (line:match("date stopped") or line:match("date started")) then
+                steps = steps + 1
+            end
+            if steps == 2 then
+                break
+            end
+
+            if os.time() > startTime + 10 then
+                return false, "Service did not stop in time"
+            end
+        end
+
+        return true
+    end):result()
+    test.assert(result, err)
+end
+
 
 if not TEST then
     test.summary()
--- tests/progress.lua
+++ tests/progress.lua
@@ -1,15 +1,12 @@
 local test = TEST or require "u-test"
 local new_test_env = require "common.test-env"
 
-test["asctl - start"] = function()
+test["asctl - restart"] = function()
     ---@type AscendTestEnvOptions
     local options = {
         services = {
             ["date"] = {
                 sourcePath = "assets/services/simple-date.hjson",
-                definition = {
-                    autostart = false,
-                }
             }
         },
         assets = {
@@ -20,25 +17,6 @@ test["asctl - start"] = function()
     local result, err = new_test_env(options):run(function(env, ascendOutput)
         local startTime = os.time()
 
-        -- while true do
-        --     print(ascendOutput:read("l"))
-        -- end
-
-        while true do
-            local line = ascendOutput:read("l", 1, "s")
-            if line and line:match("date started") then
-                return false, "Service started automatically"
-            end
-            if os.time() > startTime + 5 then
-                break
-            end
-        end
-
-        local ok, outputOrError = env:asctl({ "start", "date" })
-        if not ok then
-            return false, outputOrError
-        end
-
         while true do -- wait for service started
             local line = ascendOutput:read("l")
             if line and line:match("date started") then
@@ -46,6 +24,27 @@ test["asctl - start"] = function()
             end
             if os.time() > startTime + 10 then
                 return false, "Service did not start in time"
+            end
+        end
+
+        -- stop the service
+        local ok, outputOrError = env:asctl({ "restart", "date" })
+        if not ok then
+            return false, outputOrError
+        end
+
+        local steps = 0 -- at 2 steps we consider test completed (stop + start)
+        while true do   -- wait for service stopped
+            local line = ascendOutput:read("l")
+            if line and (line:match("date stopped") or line:match("date started")) then
+                steps = steps + 1
+            end
+            if steps == 2 then
+                break
+            end
+
+            if os.time() > startTime + 10 then
+                return false, "Service did not stop in time"
             end
         end
 
