8b8776f11f9f6deb9504faa3502419af39eedfb8
ciripel <nedelcu.alexandru@yahoo.com>
2024-09-26 16:30:01 +0300

    wip

--- tests/core-multi.lua
+++ tests/core-multi.lua
@@ -99,6 +99,59 @@ test["core - multi module - manual start"] = function()
     test.assert(result, err)
 end
 
+local test = TEST or require "u-test"
+local new_test_env = require "common.test-env"
+
+test["core - multi module - delayed start"] = function()
+    ---@type AscendTestEnvOptions
+    local options = {
+        services = {
+            ["multi"] = {
+                source_path = "assets/services/multi-module-start-delay.hjson",
+            },
+        },
+        assets = {
+            ["scripts/date.lua"] = "assets/scripts/date.lua",
+            ["scripts/one-time.lua"] = "assets/scripts/one-time.lua",
+        }
+    }
+    local result, err = new_test_env(options):run(function(env, ascendOutput)
+        local startTime = os.time()
+
+        local services = 0
+        while true do -- wait for service started
+            local line = ascendOutput:read("l")
+            if line and line:match("multi:one started %(delayed%)") then
+                services = services + 1
+            end
+            if line and line:match("multi:date started %(delayed%)") then
+                services = services + 1
+            end
+
+            if services == 2 then
+                break
+            end
+            if os.time() > startTime + 10 then
+                return false, "Service did not start in time"
+            end
+        end
+
+        -- check log exists
+        local logDir = env:get_log_dir()
+        local logFile = path.combine(logDir, "multi/date.log")
+        while true do
+            local logContent = fs.read_file(logFile)
+            if logContent and logContent:match("date:") and logContent:match("service start") then
+                break
+            end
+            if os.time() > startTime + 10 then
+                return false, "Service did not write to log in time"
+            end
+        end
+        return true
+    end):result()
+    test.assert(result, err)
+end
 
 test["core - multi module - stop"] = function()
     ---@type AscendTestEnvOptions
--- tests/progress.lua
+++ tests/progress.lua
@@ -1,43 +1,33 @@
 local test = TEST or require "u-test"
 local new_test_env = require "common.test-env"
 
-test["asctl - reload"] = function()
+test["core - multi module - delayed start"] = function()
     ---@type AscendTestEnvOptions
     local options = {
         services = {
-            ["one"] = {
-                source_path = "assets/services/simple-one-time.hjson",
-                definition = {
-                    restart = "never",
-                }
-            }
+            ["multi"] = {
+                source_path = "assets/services/multi-module-start-delay.hjson",
+            },
         },
         assets = {
-            ["scripts/one-time.lua"] = "assets/scripts/one-time.lua"
+            ["scripts/date.lua"] = "assets/scripts/date.lua",
+            ["scripts/one-time.lua"] = "assets/scripts/one-time.lua",
         }
     }
-
     local result, err = new_test_env(options):run(function(env, ascendOutput)
         local startTime = os.time()
 
-        ---@type AscendTestEnvOptions
-        local newOptions = {
-            services = {
-                ["one"] = {
-                    source_path = "assets/services/simple-one-time.hjson",
-                    definition = {
-                        restart = "always",
-                    }
-                }
-            },
-            assets = {
-                ["scripts/one-time.lua"] = "assets/scripts/one-time.lua"
-            }
-        }
-
+        local services = 0
         while true do -- wait for service started
             local line = ascendOutput:read("l")
-            if line and line:match("one started") then
+            if line and line:match("multi:one started %(delayed%)") then
+                services = services + 1
+            end
+            if line and line:match("multi:date started %(delayed%)") then
+                services = services + 1
+            end
+
+            if services == 2 then
                 break
             end
             if os.time() > startTime + 10 then
@@ -45,29 +35,18 @@ test["asctl - reload"] = function()
             end
         end
 
-        -- update the environment with new options
-        local ok, err = env:update_env(newOptions)
-        if not ok then
-            return false, err
-        end
-
-        -- reload the service
-        local ok, outputOrError = env:asctl({ "reload", "one" })
-        if not ok then
-            return false, outputOrError
-        end
-
+        -- check log exists
+        local logDir = env:get_log_dir()
+        local logFile = path.combine(logDir, "multi/date.log")
         while true do
-            local line = ascendOutput:read("l", 2)
-            if line and line:match("restarting one:default") then
+            local logContent = fs.read_file(logFile)
+            if logContent and logContent:match("date:") and logContent:match("service start") then
                 break
             end
-
             if os.time() > startTime + 10 then
-                return false, "Service did not restart in time"
+                return false, "Service did not write to log in time"
             end
         end
-
         return true
     end):result()
     test.assert(result, err)
