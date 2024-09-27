35534792a605f321b6e423c111841fcdb6c544c2
ciripel <nedelcu.alexandru@yahoo.com>
2024-09-27 22:03:00 +0300

    wip working directory

--- tests/progress.lua
+++ tests/progress.lua
@@ -1,20 +1,20 @@
 local test = TEST or require "u-test"
 local new_test_env = require "common.test-env"
 
-test["core - multi module - stop timeout (kill)"] = function()
+test["core - single module - working directory"] = function()
     ---@type AscendTestEnvOptions
     local options = {
         services = {
-            ["multi"] = {
-                source_path = "assets/services/multi-module-ignore-sigterm.hjson",
+            ["workingDir"] = {
+                source_path = "assets/services/simple-working-dir.hjson",
                 definition = {
-                    restart = "never",
+                    working_directory = "environments",
+
                 }
             }
         },
         assets = {
-            ["scripts/date.lua"] = "assets/scripts/date.lua",
-            ["scripts/ignore-sigterm.lua"] = "assets/scripts/ignore-sigterm.lua"
+            ["scripts/working-dir.lua"] = "assets/scripts/working-dir.lua"
         }
     }
     local result, err = new_test_env(options):run(function(env, ascendOutput)
@@ -22,7 +22,8 @@ test["core - multi module - stop timeout (kill)"] = function()
 
         while true do -- wait for service started
             local line = ascendOutput:read("l", 2)
-            if line and line:match("multi:ignoreSigterm started") then
+            print(line)
+            if line and line:match("workingDir:default started") then
                 break
             end
             if os.time() > startTime + 10 then
@@ -30,22 +31,22 @@ test["core - multi module - stop timeout (kill)"] = function()
             end
         end
 
-        -- stop the service
-        local ok, outputOrError = env:asctl({ "stop", "multi:ignoreSigterm" })
-        if not ok then
-            return false, outputOrError
-        end
 
+        print(options.services.workingDir.definition.working_directory) --returns nil if i do not define it in definitition
+
+        -- check log exists
+        local logDir = env:get_log_dir()
+        local logFile = path.combine(logDir, "workingDir/default.log")
         while true do
-            local line = ascendOutput:read("l", 2)
-            if line and line:match("multi:ignoreSigterm stopped %(killed%)") then
-                break
-            end
-            if os.time() > startTime + 20 then
-                return false, "Service did not stop in time"
-            end
+            local logContent = fs.read_file(logFile)
+            -- print(logContent)
+            -- if logContent and logContent:match("date:") and logContent:match("service start") then
+            --     break
+            -- end
+            -- if os.time() > startTime + 10 then
+            --     return false, "Service did not write to log in time"
+            -- end
         end
-
         return true
     end):result()
     test.assert(result, err)
