84855864bc49715845d0267b3c68d87d3a780062
ciripel <nedelcu.alexandru@yahoo.com>
2024-09-27 12:48:05 +0300

    working directory - progress test

--- tests/progress.lua
+++ tests/progress.lua
@@ -1,17 +1,15 @@
 local test = TEST or require "u-test"
 local new_test_env = require "common.test-env"
 
-test["core - single module - automatic start"] = function()
+test["core - single module - working_directory"] = function()
     ---@type AscendTestEnvOptions
     local options = {
         services = {
             ["date"] = {
                 source_path = "assets/services/simple-date.hjson",
                 definition = {
-                    working_dir = "tmp", --- in case of this service it does not make a difference
-                    restart = "always",
-                    restart_delay = 5,
-                    -- log_file = "none" -- inherits stdout/stderr
+                    working_directory = "environments",
+
                 }
             }
         },
@@ -19,10 +17,6 @@ test["core - single module - automatic start"] = function()
             ["scripts/date.lua"] = "assets/scripts/date.lua"
         }
     }
-
-    local first_env_dir = ""
-    local second_env_dir = ""
-
     local result, err = new_test_env(options):run(function(env, ascendOutput)
         local startTime = os.time()
 
@@ -36,27 +30,7 @@ test["core - single module - automatic start"] = function()
             end
         end
 
-        first_env_dir = env:get_path()
-        return true
+        return os.cwd():match("environments")
     end):result()
-
-    local result2, err2 = new_test_env(options):run(function(env, ascendOutput)
-        local startTime = os.time()
-
-        while true do -- wait for service started
-            local line = ascendOutput:read("l", 2)
-            if line and line:match("date:default started") then
-                break
-            end
-            if os.time() > startTime + 10 then
-                return false, "Service did not start in time"
-            end
-        end
-
-        second_env_dir = env:get_path()
-        return true
-    end)
     test.assert(result, err)
-    test.assert(result2, err2)
-    test.assert(first_env_dir ~= second_env_dir, "slice isolation failed")
 end
